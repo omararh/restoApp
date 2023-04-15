1ef1bd51a704b037e5bc0891c9d0d8b2cc042376
omararh <omararharbi77@gmail.com>
2023-04-15 17:30:58 +0200

    “APP”

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -5,11 +5,6 @@ import '../pages/cart.css';
 const Cart = ({cartItems}) => {
     return (
         <div>
-           {
-               cartItems.map(e => {
-                   <p>e.id</p>
-               }) 
-           }
             <p>ici PANIER!</p>
         </div>
     )
